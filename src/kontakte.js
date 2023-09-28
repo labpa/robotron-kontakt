7a26272735ce1cee0307fd4a0b7e10843330011d
Aramís Concepción Durán <aramis@systemli.org>
2023-09-28 17:56:57 +0200

    Suchfunktion

--- src/kontakte.js
+++ src/kontakte.js
@@ -1,23 +1,23 @@
 import { createTable, forceLogin, supabase } from "./util.js";
 
 forceLogin();
-const { data, error } = await supabase.from('Kontakte').select('*');
+const { data, error } = await supabase.from("Kontakte").select("*");
 createTable(data);
-document.querySelector("form#formSearch").onsubmit = (ev) => {
-	ev.preventDefault();
-	let filteredData = data;
-	const searchInput = document.querySelector("input#searchInput").value.toLowerCase().split(" ");
-	for (const i in searchInput) {
-    	const word = searchInput[i].trim(); 
-    	filteredData = filteredData.filter(row => {
+document.querySelector("form#formSearch").onsubmit = ev => {
+    ev.preventDefault();
+    let filteredData = data;
+    const searchInput = document.querySelector("input#searchInput").value.toLowerCase().split(" ");
+    for (const i in searchInput) {
+        const word = searchInput[i].trim(); 
+        filteredData = filteredData.filter(row => {
             for (const i in row){
-            	const cell = String(row[i]).toLowerCase();
-            	if (cell.includes(word)) {
-                	return true;
-            	}
+                const cell = String(row[i]).toLowerCase();
+                if (cell.includes(word)) {
+                    return true;
+                }
             }
             return false;
-    	});
-	}
-	createTable(filteredData);
+        });
+    }
+    createTable(filteredData);
 }
