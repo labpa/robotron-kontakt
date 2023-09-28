f04bd876f5b129179899752a965f6f4d1a529597
probstv <probstv@PC8061>
2023-09-28 10:43:25 +0200

    refactored Kontakte again. Jede Funktion ist einzeln

--- src/kontakte.js
+++ src/kontakte.js
@@ -16,51 +16,51 @@ function createTable(data, isWithViewButton=true) {
 		createTableBody(data, table);
 		div.appendChild(table);
 	}
+}
 
-	// helper functions for createTable()
-	function createTableHead(data, table, isWithViewButton=true) {
-		const thead = document.createElement("thead");
-		const trhead = document.createElement("tr");
-		const thView = document.createElement("th");
-		if (isWithViewButton) {
-			thView.appendChild(document.createTextNode("Ansehen"));
-			trhead.appendChild(thView);
-		}
-		
-		for (const key in data[0]) {
-			const th = document.createElement("th");
-			th.appendChild(document.createTextNode(key));
-			trhead.appendChild(th);
-		}
-		table.appendChild(thead);
-		thead.appendChild(trhead);
+// helper functions for createTable()
+function createTableHead(data, table, isWithViewButton=true) {
+	const thead = document.createElement("thead");
+	const trhead = document.createElement("tr");
+	const thView = document.createElement("th");
+	if (isWithViewButton) {
+		thView.appendChild(document.createTextNode("Ansehen"));
+		trhead.appendChild(thView);
 	}
+	
+	for (const key in data[0]) {
+		const th = document.createElement("th");
+		th.appendChild(document.createTextNode(key));
+		trhead.appendChild(th);
+	}
+	table.appendChild(thead);
+	thead.appendChild(trhead);
+}
 
-	function createTableBody(data, table, isWithViewButton=true) {
-		const tbody = document.createElement("tbody");
-		for (const row of data) {
-			const tr = document.createElement("tr");
-			
-			if (isWithViewButton) createViewButton(tr);
-							
-			for (const cell in row) {
-				const td = document.createElement("td");
-				td.dataset.label = cell;
-				td.appendChild(document.createTextNode(row[cell]));
-				tr.appendChild(td);
-			}
-			tbody.appendChild(tr);
-		}
-		table.appendChild(tbody);
+function createTableBody(data, table, isWithViewButton=true) {
+	const tbody = document.createElement("tbody");
+	for (const row of data) {
+		const tr = document.createElement("tr");
 		
-		// helper function for createViewButton()
-		function createViewButton(tr) {
-			const buttonView = document.createElement("button");
-			buttonView.appendChild(document.createTextNode("View"));
-			buttonView.addEventListener("click", ()=>window.open("./view.html?id=" + row.id, "_self"));
-			const tdView = document.createElement("td");
-			tdView.appendChild(buttonView);
-			tr.appendChild(tdView);
+		if (isWithViewButton) createViewButton(tr);
+						
+		for (const cell in row) {
+			const td = document.createElement("td");
+			td.dataset.label = cell;
+			td.appendChild(document.createTextNode(row[cell]));
+			tr.appendChild(td);
 		}
+		tbody.appendChild(tr);
 	}
+	table.appendChild(tbody);
+}
+
+// helper function for createViewButton()
+function createViewButton(tr) {
+	const buttonView = document.createElement("button");
+	buttonView.appendChild(document.createTextNode("View"));
+	buttonView.addEventListener("click", ()=>window.open("./view.html?id=" + row.id, "_self"));
+	const tdView = document.createElement("td");
+	tdView.appendChild(buttonView);
+	tr.appendChild(tdView);
 }
