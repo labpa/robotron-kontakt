085ed5b6d5b7c330044e38571a9a23ca6077f990
Johannes Berger <bergerj@myrobotron.de>
2023-10-10 09:32:57 +0200

    Checkbox für Datenverarbeitungszustimmung sowie CSS für Checkbox und Footer

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ contactForm.onsubmit = async (ev)=>{
 	for (const entry of entries) {
 		submitData[entry[0]] = entry[1] === "" ? null : entry[1];
 	}
-	
+
 	const session = await supabase.auth.getSession();
 	if (session.data.session !== null) {
 		submitData["Creator"] = session.data.session.user.email;
 	}
-	
+
 	const { data, error } = await supabase
 		.from('Kontakte')
 		.insert([submitData]);
