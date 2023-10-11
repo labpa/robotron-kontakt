165a2db7d11f4605f139dd49b52c93f277d8d2c7
Aramís Concepción Durán <aramis@systemli.org>
2023-10-11 10:17:33 +0200

    build scripts

--- publish-hut.fish
+++ publish-hut.fish
@@ -1,9 +1,6 @@
 #!/usr/bin/env fish
-
-npm install
 npm run build
 tar -C dist -cvz . > site.tar.gz
 hut pages publish -d robotron.srht.site site.tar.gz
 rm site.tar.gz
-rm -rf node_modules
 rm -rf dist
--- publish-nojs.fish
+++ publish-nojs.fish
@@ -1,7 +1,4 @@
 #!/usr/bin/env fish
-
-npm install
 npm run build
-
 cd dist
 scp -r * root@hub.nojs.de:/var/www/robotron-kontakt.nojs.de/
